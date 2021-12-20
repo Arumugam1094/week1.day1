a5a13cad826a7c7ea2a3e23e399c57de4b7dd4e2
Arumugam1094 <94920349+Arumugam1094@users.noreply.github.com>
2021-12-20 14:41:26 +0530

    Add files via upload

---
+++ week3/day2/SBI.java
@@ -0,0 +1,40 @@
+package week3.day2;
+
+public class SBI implements RBI{
+	
+	public void ITLoan() {
+		System.out.println("100000");
+
+	}
+
+	public void minimumBalance() {
+		System.out.println("2000");		
+	}
+
+	public void maximumLoanAmount() {
+		System.out.println("200000");
+		
+	}
+	
+	public static void main(String[] args) {
+		IOB iob = new IOB();
+		iob.minimumBalance();
+		iob.maximumLoanAmount();
+		iob.agriLoan();
+		
+		
+		
+		/*
+		 * SBI sbi = new SBI();
+		 * 
+		 * sbi.minimumBalance(); sbi.maximumLoanAmount(); sbi.ITLoan();
+		 */
+		  
+	}
+
+	public void repoRate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
