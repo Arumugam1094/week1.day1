87236942a027b43cb25e0c9da9eb18fcfcf68230
Arumugam1094 <94920349+Arumugam1094@users.noreply.github.com>
2021-12-20 14:42:34 +0530

    Add files via upload

---
+++ week5/day1/Parallel.java
@@ -0,0 +1,16 @@
+package week5.day1;
+
+public class Parallel {
+	
+	/*<?xml version="1.0" encoding="UTF-8"?>
+	<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd">
+	<suite thread-count="2" parallel="classes" name="Suite">
+	  <test name="Test">
+	    <classes>
+	      <class name="week5.day1.CreateLead"/>
+	      <class name="week5.day1.DeleteLead"/>
+	    </classes>
+	  </test> <!-- Test -->
+	</suite> <!-- Suite -->*/
+
+}
